f5d8d4e659edd96c1dae718958ab75f367377425
Nitish Choudhary <nitishchy007@gmail.com>
2022-02-06 12:50:44 +0530

    intro set without animations

--- movie-hub/src/components/homePageIntro/HomePageIntro.js
+++ movie-hub/src/components/homePageIntro/HomePageIntro.js
@@ -1,28 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import "./homepageintro.css"
-import axios from 'axios';
-import logo from "../../utilities/image.jpg"
-
+import React, { useEffect, useState } from "react";
+import "./homepageintro.css";
+import axios from "axios";
+import logo from "../../utilities/image.jpg";
+import logo1 from "../../utilities/image1.jpg";
+import logo2 from "../../utilities/image2.jpg";
+import logo3 from "../../utilities/image3.jpg";
+import logo4 from "../../utilities/image4.jpg";
 
 export default function HomePageIntro() {
-    const pics = [logo, logo, logo, logo, logo, logo, logo]
-    const [introMovies, setIntroMovies] = useState([])
-    useEffect(() => {
-        axios.get("https://imdb-api.com/en/API/MostPopularMovies/k_6nq48ps7")
-            .then(response => {
-                console.log(response.data)
-                // setIntroMovies(response.data.items.slice(0,6))
-            }, [])
+  const pics = [logo, logo1, logo2, logo3, logo4];
+  const [introMovies, setIntroMovies] = useState([]);
+  const [useInBg, setUseInBg] = useState(pics[0]);
 
-    })
-    return (
-        <div className='intro'>
-            <div className='poster-page'>
-                <div className='pip'>
-                    {pics.map(pic => <img src={pic} style={{ "height": "200px" }} />
-                    )}
-                </div>
-            </div>
+  // useEffect(() => {
+  //     axios.get("https://imdb-api.com/en/API/MostPopularMovies/k_6nq48ps7")
+  //         .then(response => {
+  //             console.log(response.data)
+  //             // setIntroMovies(response.data.items.slice(0,6))
+  //         }, [])
+
+  // })
+
+  return (
+    <div className="intro">
+      <div className="poster-page">
+        {useInBg ? <img src={useInBg} alt="moviesPoster" /> : null}
+      </div>
+      <div className="pip">
+        <p>Our Recomendations</p>
+        <div className="pip-img">
+          {pics.map((pic, index) => (
+            <img
+              src={pic}
+              alt="moviesPoster"
+              onClick={() => setUseInBg(pic)}
+              key={index}
+            />
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
